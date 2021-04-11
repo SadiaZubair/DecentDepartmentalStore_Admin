966bff2c55d0d35e70ffe851271403065cc8645a
ayesha2325 <77952410+ayesha2325@users.noreply.github.com>
2021-04-11 18:59:47 +0500

    Add files via upload

---
+++ src/components/Admindetails.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(5),
+      width: '25ch',
+    },
+  },
+}));
+
+export default function Admindetails() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      {/* <TextField id="standard-basic" style={{width:'50ch'}} label="Standard" />
+      <TextField id="filled-basic" style={{width:'50ch'}} label="Filled" variant="filled" /> */}
+       <Typography variant="h6" className={classes.title}>
+            Admin Details
+          </Typography>
+          
+          <ListGroup horizontal>  
+      <div>
+      <TextField id="filled-basic" style={{marginRight:'20px', width: '24ch'}} label="First Name" variant="filled" />
+      </div>
+      <div>
+      <TextField id="filled-basic" style={{ width: '24ch'}} label="Last Name" variant="filled" />
+      </div>
+      </ListGroup> 
+      <div>
+      <TextField id="filled-basic" style={{width:'50ch'}} label="Phone Number" variant="filled" />
+      </div>
+      <div>
+      <TextField id="filled-basic" style={{width:'50ch'}} label="Email Address" variant="filled" />
+      </div>
+      <button type="button" style={{backgroundColor: '#0277BD', color: '#FFFFFF', width: '150px'}} className={classes.thecolor} class="btn back-color rounded-pill">Update</button>
+      <button type="button" style={{backgroundColor: '#0277BD', color: '#FFFFFF', width: '150px'}} className={classes.thecolor} class="btn back-color rounded-pill">Cancel</button>
+    </form>
+  );
+}
