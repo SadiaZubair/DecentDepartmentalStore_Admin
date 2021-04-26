32216457bbc9977afd5f412376b51315d1def9ad
rukhshan23 <74811379+rukhshan23@users.noreply.github.com>
2021-04-26 19:04:55 +0500

    Add files via upload

--- src/components/ForgotPasswordAdmin.js
+++ src/components/ForgotPasswordAdmin.js
@@ -135,7 +135,7 @@ export default function AdminLogin() {
         
         <form className={classes.form} noValidate>
           <TextField
-            variant="outlined"
+          
             margin="normal"
             required
             fullWidth
