a5f36c6dd659e11acf04df851925108fad805010
ayesha2325 <77952410+ayesha2325@users.noreply.github.com>
2021-04-26 19:12:15 +0500

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 import styled from 'styled-components';
-import final from './final.png'
+import {Link} from 'react-router-dom';
+import final from './final.png';
+import {useHistory} from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import FacebookIcon from '@material-ui/icons/Facebook';
 //import './Navbar.css'
@@ -9,7 +11,7 @@ import Dropdown from './Dropdown'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Button from 'react-bootstrap/Button';
 import fire from '../fire'
-
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 const Styles = styled.div`
   .navbar { background-color: #004C8C; }
   a, .navbar-nav, .navbar-light .nav-link {
@@ -32,9 +34,11 @@ const Styles = styled.div`
 
 
 function NavBar() {
+  let history=useHistory();
 const handleLogout = () => {
+  history.push("/")
   fire.auth().signOut().then(() => {
-          
+        
   }).catch((error) => {
 // An error happened.
   });
@@ -47,14 +51,14 @@ const handleLogout = () => {
     <Navbar expand="lg">
       <img src={final} alt="" height = "43" width ="65"/>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Form className="form-center">
+      {/* <Form className="form-center">
         <FormControl type="text" placeholder="Search" className="" />
-      </Form>
+      </Form> */}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-        
+        <Nav.Item className='nav-itempro' marginRight='20px' ><Link to={"/View Admin"}>< AccountBoxIcon fontSize="large"  /></Link></Nav.Item>
           <Nav.Item  class="logout"> 
-          <Button backgroundColor='#0277BD' onClick={handleLogout} color="primary"> Log Out</Button>
+          <Button styles={{ textDecorationColor:' #ffffff'}} onClick={handleLogout} className="adjustSubmit"> Sign Out</Button>
           </Nav.Item> 
          
         </Nav>
