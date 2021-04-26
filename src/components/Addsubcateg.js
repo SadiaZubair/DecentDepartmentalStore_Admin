e9c20aacc4eb32fca90f9f32c08df836fea45923
shezanaveed <77901748+shezanaveed@users.noreply.github.com>
2021-04-26 19:15:34 +0500

    Add files via upload

--- src/components/Addsubcateg.js
+++ src/components/Addsubcateg.js
@@ -7,7 +7,12 @@ import {Row, Col} from 'react-bootstrap';
 import IconButton from '@material-ui/core/IconButton';
 import UploadButtons from './Uploadbutton';
 import { useState } from "react";
+import {useHistory} from 'react-router-dom';
 import fire from "../fire";
+import { LaptopWindows } from '@material-ui/icons';
+import Snackbar from '@material-ui/core/Snackbar';
+
+import CloseIcon from '@material-ui/icons/Close';
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -16,14 +21,55 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
+const useStylesbutton = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  // input: {
+  //   display: 'none',
+  // },
+}));
 
 export default function Addsubcateg({match}) {
+  let history=useHistory();
   let category=match.params.category;
   const [subcatname, setSubcatname]=useState();
   const classes = useStyles();
-  
- 
-  
+  const classesbutton = useStylesbutton();
+
+  const [error1, setError1] = useState('');
+  const [opensnack, setOpensnack] = React.useState(false);
+  const handleClosesnack = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpensnack(false);
+    history.push('/Categories/'+ category)
+  };
+    const types = ['image/png', 'image/jpeg']; // image types
+
+    const [productImg, setProductImg] = useState(null)
+  var img;
+    const productImgHandler = (e) => {
+        let selectedFile = e.target.files[0];
+        if (selectedFile && types.includes(selectedFile.type)) {
+            setProductImg(selectedFile);
+           
+            img=selectedFile
+            console.log(productImg)
+            setError1('')
+        }
+        else {
+            setProductImg(null);
+            setError1('Please select a valid image type (jpg or png)');
+            window.alert("Please select a valid image type (jpg or png)")
+        }
+    }
+    const [error, setError] = useState('');
+    const storage = fire.storage();
 
 
 		const addsubcat = (category,subcategory)=>{
@@ -41,11 +87,31 @@ export default function Addsubcateg({match}) {
         	
       		});
    			} else {
+           if(productImg== null || subcategory =='')
+           {
+            window.alert("Required Fields are missing")
+           }
+           else
+           {
    				console.log("This subcategory is not a category, hence good to go!")
+           const uploadTask = storage.ref(`product-images/${productImg.name}`).put(productImg);
+          //console.log(uploadTask)
+          uploadTask.on('state_changed', snapshot => {
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log(progress);
+          }, err => setError(err.message)
+              , () => {
+                  storage.ref('product-images').child(productImg.name).getDownloadURL().then(url => {
    				db.collection("category").doc(category).collection("subcategory").doc(subcategory).set({
             [subcategory]: subcategory,
+            imagelink: url,
             });
-            window.alert("Adding")
+           
+           setOpensnack(true);
+          })
+        })
+      }
+     
     		}
 			});
 
@@ -55,6 +121,9 @@ export default function Addsubcateg({match}) {
 
 
 		}
+    const cancel=(e)=>{
+      history.push('/Categories/'+ category)
+    }
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
@@ -71,7 +140,14 @@ export default function Addsubcateg({match}) {
       <div>
         {/* <Row>
           <Col> */}
-          <UploadButtons/>
+           <div className={classesbutton.root}>
+          
+          <label>
+            <h5>Upload Image</h5> 
+            <br/>
+      <input style={{backgroundColor:'0277BD'}} accept="image/*" className={classesbutton.input} id="contained-button-file" type="file" onChange={productImgHandler} />
+      </label>
+      </div>
           {/* </Col> */}
           {/* <Col xs={2}>
         <Typography variant="h6" className={classes.title}>
@@ -81,7 +157,25 @@ export default function Addsubcateg({match}) {
         {/* </Row> */}
           </div>
       <button type="button" onClick={function(event){addsubcat(category, subcatname)}} style={{backgroundColor: '#0277BD', color: '#FFFFFF', width: '150px'}} className={classes.thecolor} class="btn back-color rounded-pill">Add</button>
-      <button type="button" style={{backgroundColor: '#0277BD', color: '#FFFFFF', width: '150px'}} className={classes.thecolor} class="btn back-color rounded-pill">Cancel</button>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={opensnack}
+        autoHideDuration={6000}
+        onClose={handleClosesnack}
+        message="Subcategory Successfully Added"
+        action={
+          <React.Fragment>
+           
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClosesnack}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+      <button type="button" style={{backgroundColor: '#0277BD', color: '#FFFFFF', width: '150px'}}  onClick={(e)=> cancel(e)} className={classes.thecolor} class="btn back-color rounded-pill">Cancel</button>
     </form>
   );
 }
